f7b5e0e51ff98b2f8c1b191c44fde59aefb550ef
Kirilloao <kirilltaraturin747@gmail>
2023-08-17 12:48:57 +0200

    create window

---
+++ RickAndMortyTestTask/App/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  RickAndMortyTestTask
+//
+//  Created by Kirill Taraturin on 17.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
